47f82d6fcb5dacdde51209912b12bd30c589747e
Muhammad Thakeeb Muhammad <thakeebmuhammad@gmail.com>
2026-02-25 16:48:13 +0100

    Fixed image upload error

--- fairbid-backend/src/routes/auctionRoutes.js
+++ fairbid-backend/src/routes/auctionRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer")
+const path = require("path")
 const {
   createAuction,
   getAuctions,
@@ -7,6 +9,17 @@ const {
   getFairnessMetrics,
 } = require("../controllers/auctionController");
 
+// Multer setup
+const storage = multer.diskStorage({
+  destination:(req, file, cb) =>{
+    cb(null,path.join(__dirname, "../uploads"))
+  },
+  filename:(req, file, cb)=>{
+    const uniqueSuffix = Date.now()+"-"+Math.round(Math.random()*1e9)
+    cb(null,uniqueSuffix + path.extname(file.originalname))
+  },
+})
+const upload = multer({storage})
 // ✅ Static / specific routes first
 router.post("/create",upload.single("image"), createAuction);
 router.get("/:auctionId/fairness", getFairnessMetrics);
