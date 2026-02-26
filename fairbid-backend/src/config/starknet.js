46ce005c3aea1aae2635ddedc938cabc8c8655e2
Muhammad Thakeeb Muhammad <thakeebmuhammad@gmail.com>
2026-02-27 00:30:17 +0100

    try

--- fairbid-backend/src/config/starknet.js
+++ fairbid-backend/src/config/starknet.js
@@ -5,12 +5,12 @@ const RPC = process.env.STARKNET_RPC;
 const ADDRESS = process.env.ADMIN_ADDRESS;
 const PRIVATE_KEY = process.env.ADMIN_PRIVATE_KEY;
 
-// ✅ Use explicit RPC provider
+//  RPC provider
 const provider = new Provider({
   nodeUrl: RPC,
 });
 
-// ✅ Create admin account
+//  Create admin account
 const adminAccount = new Account(provider, ADDRESS, PRIVATE_KEY);
 
 module.exports = {
